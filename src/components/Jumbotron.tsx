b83fc8eae14d352cc871102d36a555d2f1c09d20
unknown <anumahjoshuaeneye@gmail.com>
2024-03-13 12:24:15 +0100

    jumbotron done

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -1,9 +1,9 @@
-import { Button, Heading } from ".";
+import { Button, Heading, Img } from ".";
 
 function Jumbotron() {
   return (
-    <div className="max-w-screen-lg bg-emerald-200 mx-auto rounded-md md:rounded-lg lg:rounded-xl my-8 md:my-16 lg:my-20 flex flex-col md:flex-row px-8 md:px-16 ">
-      <div className="flex-1 flex flex-col justify-between gap-8 md:gap-12 py-8 md:py-16">
+    <div className="max-w-screen-lg bg-emerald-200 mx-auto rounded-md md:rounded-lg lg:rounded-xl my-8 md:my-16 lg:my-20 flex flex-col md:flex-row px-8 md:px-16 overflow-hidden relative">
+      <div className="w-7/12 flex flex-col justify-between gap-8 md:gap-12 py-8 md:py-16">
         <div>
           <Heading size="6xl">Maximize your business with Jowis</Heading>
           <Heading size="lg" className="text-gray-600">
@@ -28,6 +28,14 @@ function Jumbotron() {
             About Us
           </Button>
         </div>
+      </div>
+      <div className="w-5/12">
+        <Img alt="" className="w-full" src="/assets/img_group_1.png" />
+        <Img
+          alt=""
+          className="absolute bottom-0 right-0 w-3/12 hidden md:block"
+          src="../../public/assets/img_web_1.png"
+        />
       </div>
     </div>
   );
