6e5b7f69a35a03e9165d59a7db1e4a7b113bd105
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-04-11 11:13:50 +0100

    sidebar closing hot-fix

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,10 @@
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
-import { Link } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 function SideBar() {
+  const navigate = useNavigate();
   return (
     <Sheet>
       <SheetTrigger>
@@ -10,10 +12,67 @@ function SideBar() {
       </SheetTrigger>
       <SheetContent>
         <div className="h-full justify-center items-center flex-col flex text-3xl gap-8">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/work">Work</Link>
-          <Link to="/contact">Contact</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              (isActive
+                ? "text-red-400"
+                : "text-black dark:text-white hover:underline") +
+              " capitalize  decoration-2 underline-offset-4"
+            }
+            onClick={() => {
+              navigate("/");
+              window.location.reload();
+            }}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              (isActive
+                ? "text-red-400"
+                : "text-black dark:text-white hover:underline") +
+              " capitalize  decoration-2 underline-offset-4"
+            }
+            onClick={() => {
+              navigate("/about");
+              window.location.reload();
+            }}
+          >
+            about
+          </NavLink>
+          <NavLink
+            to="/work"
+            className={({ isActive }) =>
+              (isActive
+                ? "text-red-400"
+                : "text-black dark:text-white hover:underline") +
+              " capitalize  decoration-2 underline-offset-4"
+            }
+            onClick={() => {
+              navigate("/work");
+              window.location.reload();
+            }}
+          >
+            work
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              (isActive
+                ? "text-red-400"
+                : "text-black dark:text-white hover:underline") +
+              " capitalize  decoration-2 underline-offset-4"
+            }
+            onClick={() => {
+              navigate("/contact");
+              window.location.reload();
+            }}
+          >
+            contact
+          </NavLink>
         </div>
       </SheetContent>
     </Sheet>
